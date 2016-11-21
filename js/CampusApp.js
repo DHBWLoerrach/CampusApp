50045c10485ed3e1666a94ca0945fb8a86dd592d
Erik Behrends <erik.behrends@gmail.com>
2016-11-21 17:13:17 +0100

    Translucent and light status status bar

--- js/CampusApp.js
+++ js/CampusApp.js
@@ -2,10 +2,31 @@
 'use strict';
 
 import React, { Component } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+
 import TabsView from './TabsView';
 
 export default class CampusApp extends Component {
   render() {
-    return (<TabsView/>);
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          translucent={true}
+          backgroundColor="rgba(0, 0, 0, 0.2)"
+          barStyle="light-content"
+         />
+        <TabsView/>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
