48fdc04bf77145e18eaabc42fe07f65a3dc5526b
Erik Behrends <erik.behrends@gmail.com>
2016-11-22 11:15:51 +0100

    iOS: Add support for left/right action item in header

--- js/util/CampusHeader.ios.js
+++ js/util/CampusHeader.ios.js
@@ -3,21 +3,41 @@
 
 import React, { Component } from 'react';
 import {
+  Image,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 import Colors from './Colors.js';
 
 export default class CampusHeader extends Component {
+  _renderActionItem(actionItem) {
+      if(actionItem) {
+        return(
+          <TouchableOpacity onPress={actionItem.onPress}>
+            <Image source={actionItem.icon}/>
+          </TouchableOpacity>
+        );
+      } else {
+        return null;
+      }
+  }
+
   render() {
     return(
       <View style={styles.header}>
+        <View style={styles.leftActionItem}>
+          {this._renderActionItem(this.props.leftActionItem)}
+        </View>
         <View style={styles.headerTitle}>
           <Text style={styles.titleText}>
             {this.props.title}
           </Text>
+        </View>
+        <View style={styles.rightActionItem}>
+          {this._renderActionItem(this.props.rightActionItem)}
         </View>
       </View>
     );
@@ -37,10 +57,18 @@ const styles = StyleSheet.create({
     flex: 2,
     alignItems: 'center',
   },
+  leftActionItem: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
   titleText: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 36,
     letterSpacing: -1,
+  },
+  rightActionItem: {
+    flex: 1,
+    alignItems: 'flex-end',
   },
 });
