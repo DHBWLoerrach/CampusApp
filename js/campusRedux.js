ec04efce8d621176bacd0761dc309d08c1713abb
Erik Behrends <behrends@users.noreply.github.com>
2019-08-15 18:22:45 +0200

    Upgrade redux-persist to version 5.10.0

--- js/campusRedux.js
+++ js/campusRedux.js
@@ -1,32 +1,31 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import {
-  applyMiddleware,
-  combineReducers,
-  compose,
-  createStore
-} from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk'; // thunk middleware to use functions as actions
-import { persistStore, autoRehydrate } from 'redux-persist';
+import { persistStore, persistCombineReducers } from 'redux-persist';
 import { news } from './tabs/news/redux';
 import { schedule } from './tabs/schedule/redux';
 import { canteen } from './tabs/canteen/redux';
 import { settings } from './tabs/service/redux';
 
-// SETUP STORE with middleware
+export default function setupStore(onComplete) {
+  const config = {
+    key: 'primary',
+    storage: AsyncStorage
+  };
 
-export default function setupStore(onComplete: ?() => void) {
-  const reducers = combineReducers({
+  const reducers = persistCombineReducers(config, {
     news,
     schedule,
     canteen,
     settings
   });
-  // enhance store: autohydrate (offline data), thunk middleware (functions actions)
-  const storeEnhancers = compose(
-    autoRehydrate(),
-    applyMiddleware(thunk)
+
+  const store = createStore(
+    reducers,
+    undefined,
+    compose(applyMiddleware(thunk))
   );
-  const store = createStore(reducers, {}, storeEnhancers);
-  persistStore(store, { storage: AsyncStorage }, onComplete);
+
+  persistStore(store, null, onComplete);
   return store;
 }
