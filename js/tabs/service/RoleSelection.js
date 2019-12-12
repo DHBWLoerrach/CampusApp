e45478887e1fc164237bd5ad82d1a92095761c4e
Erik Behrends <behrends@users.noreply.github.com>
2019-12-12 17:15:38 +0100

    Refactoring: use functions in role selection

--- js/tabs/service/RoleSelection.js
+++ js/tabs/service/RoleSelection.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Platform,
   StyleSheet,
@@ -13,57 +13,34 @@ import { roles } from '../../util/Constants';
 const RadioButtonTouchable =
   Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
 
-class RadioButton extends Component {
-  render() {
-    return (
-      <RadioButtonTouchable onPress={this.props.onPress}>
-        <View style={styles.radioButton}>
-          <View style={styles.outerCircle}>
-            {this.props.selected ? (
-              <View style={styles.innerCircle} />
-            ) : null}
-          </View>
-          <Text
-            style={[
-              styles.label,
-              this.props.selected ? styles.bold : null
-            ]}
-          >
-            {this.props.label}
-          </Text>
+function RadioButton(props) {
+  return (
+    <RadioButtonTouchable onPress={props.onPress}>
+      <View style={styles.radioButton}>
+        <View style={styles.outerCircle}>
+          {props.selected ? <View style={styles.innerCircle} /> : null}
         </View>
-      </RadioButtonTouchable>
-    );
-  }
+        <Text style={[styles.label, props.selected ? styles.bold : null]}>
+          {props.label}
+        </Text>
+      </View>
+    </RadioButtonTouchable>
+  );
 }
 
-export default class RoleSelection extends Component {
-  render() {
-    return (
-      <View>
+export default function RoleSelection(props) {
+  return (
+    <View>
+      {roles.map((role, index) => (
         <RadioButton
-          selected={this.props.role === roles[0]}
-          label={roles[0]}
-          onPress={() => this.props.onRoleChange(roles[0])}
+          key={index}
+          selected={props.role === role}
+          label={role}
+          onPress={() => props.onRoleChange(role)}
         />
-        <RadioButton
-          selected={this.props.role === roles[1]}
-          label={roles[1]}
-          onPress={() => this.props.onRoleChange(roles[1])}
-        />
-        <RadioButton
-          selected={this.props.role === roles[2]}
-          label={roles[2]}
-          onPress={() => this.props.onRoleChange(roles[2])}
-        />
-        <RadioButton
-          selected={this.props.role === roles[3]}
-          label={roles[3]}
-          onPress={() => this.props.onRoleChange(roles[3])}
-        />
-      </View>
-    );
-  }
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
