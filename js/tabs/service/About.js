0f0bb55cbeee642ebc26e0bee5061bc73dcab56e
Erik Behrends <behrends@users.noreply.github.com>
2020-05-15 22:34:05 +0200

    Bump version to v.2.4.1

--- js/tabs/service/About.js
+++ js/tabs/service/About.js
@@ -27,7 +27,7 @@ export default class About extends Component {
         >
           github.com/DHBWLoerrach/CampusApp
         </Text>
-        <Text style={styles.marginBig}>Version (App): 2.4.0</Text>
+        <Text style={styles.marginBig}>Version (App): 2.4.1</Text>
       </ScrollView>
     );
   }
