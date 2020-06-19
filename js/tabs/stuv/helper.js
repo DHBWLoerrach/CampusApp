974066bece0707d3beb025a3b022f4d2910eaf71
Erik Behrends <behrends@users.noreply.github.com>
2020-06-19 16:32:06 +0200

    StuV: replace moment.js with date-fns

--- js/tabs/stuv/helper.js
+++ js/tabs/stuv/helper.js
@@ -1,37 +1,40 @@
-import moment from "moment";
+import { format } from 'date-fns';
+import fromUnixTime from 'date-fns/fromUnixTime';
 
 export async function loadEvents() {
-    const body = await load("https://storage.googleapis.com/stuv-data/serverResponseEvents.json");
-    return body.response;
+  const body = await load(
+    'https://storage.googleapis.com/stuv-data/serverResponseEvents.json'
+  );
+  return body.response;
 }
 
 export async function loadNews() {
-    const body = await load("https://storage.googleapis.com/stuv-data/serverResponseNews.json");
-    return body.response;
+  const body = await load(
+    'https://storage.googleapis.com/stuv-data/serverResponseNews.json'
+  );
+  return body.response;
 }
 
 async function load(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        return null;
-    }
-    return await response.json();
+  const response = await fetch(url);
+  if (!response.ok) {
+    return null;
+  }
+  return await response.json();
 }
 
 export function shortString(text, maxLength) {
-    if (text.length > maxLength) {
-        return text.substr(0, maxLength-3) + "...";
-    } else {
-        return text;
-    }
+  if (text.length > maxLength) {
+    return text.substr(0, maxLength - 3) + '...';
+  } else {
+    return text;
+  }
 }
 
 export function unixTimeToDateText(time) {
-    const date = moment(time*1000);
-    return date.format('DD.MM.Y');
+  return format(fromUnixTime(time), 'dd.MM.Y');
 }
 
 export function unixTimeToTimeText(time) {
-    const date = moment(time*1000);
-    return date.format('HH:mm');
+  return format(fromUnixTime(time), 'HH:mm');
 }
