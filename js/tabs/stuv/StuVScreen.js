dfc0a02b9c718a18f5579ea84c107b8e7b01bd02
Erik Behrends <behrends@users.noreply.github.com>
2020-06-19 15:37:28 +0200

    Format with prettier

--- js/tabs/stuv/StuVScreen.js
+++ js/tabs/stuv/StuVScreen.js
@@ -1,28 +1,28 @@
-import React from "react";
-import {StyleSheet, View} from "react-native";
-import TabbedSwipeView from "../../util/TabbedSwipeView.ios";
-import StuVEvents from "./events/StuVEvents";
-import StuVNews from "./news/StuVNews";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import TabbedSwipeView from '../../util/TabbedSwipeView.ios';
+import StuVEvents from './events/StuVEvents';
+import StuVNews from './news/StuVNews';
 
 function getPages() {
-    return [{title: "StuV-News", content: <StuVNews/>}, {title: "StuV-Events", content:  <StuVEvents/>}];
+  return [
+    { title: 'StuV-News', content: <StuVNews /> },
+    { title: 'StuV-Events', content: <StuVEvents /> },
+  ];
 }
 
 function StuVScreen() {
-
-    return (
-        <View style={styles.container}>
-            <TabbedSwipeView
-                pages={getPages()}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TabbedSwipeView pages={getPages()} />
+    </View>
+  );
 }
 
 export default StuVScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+  },
 });
