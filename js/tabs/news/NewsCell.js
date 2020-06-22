3d6e919294c53bea8fc828a45117183cddf333d3
Erik Behrends <behrends@users.noreply.github.com>
2020-06-22 14:19:40 +0200

    Rewrite NewsCell component as function

--- js/tabs/news/NewsCell.js
+++ js/tabs/news/NewsCell.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { formatDistanceToNow, format } from 'date-fns';
 import { de } from 'date-fns/locale';
 
 import CommonCell from '../../util/CommonCell';
 
-export default class NewsCell extends Component {
-  render() {
-    let image = require('../../img/logo.png');
-    // formatting for news items: relative date (e.g. "3 days ago")
-    let time = formatDistanceToNow(new Date(this.props.news.time), {
-      locale: de,
-      addSuffix: true,
-    });
-    // special formatting for events: absolute date in red
-    if (this.props.topic === 'events') {
-      time = format(new Date(this.props.news.time), 'dd.MM.yyyy');
-    }
-    if (this.props.news.imgUrl)
-      image = { uri: this.props.news.imgUrl };
-    return (
-      <CommonCell
-        title={this.props.news.heading}
-        details={[time]}
-        description={this.props.news.subheading}
-        imageSource={image}
-        onPress={() => this.props.onPress({ news: this.props.news })}
-      />
-    );
+export default ({
+  news: { time, imgUrl, heading, subheading },
+  topic,
+  onPress,
+}) => {
+  let image = require('../../img/logo.png');
+  // formatting for news items: relative date (e.g. "3 days ago")
+  let formattedTime = formatDistanceToNow(new Date(time), {
+    locale: de,
+    addSuffix: true,
+  });
+  // special formatting for events: absolute date in red
+  if (topic === 'events') {
+    formattedTime = format(new Date(time), 'dd.MM.yyyy');
   }
-}
+  if (imgUrl) image = { uri: imgUrl };
+  return (
+    <CommonCell
+      title={heading}
+      details={[formattedTime]}
+      description={subheading}
+      imageSource={image}
+      onPress={() => onPress({ news: news })}
+    />
+  );
+};
