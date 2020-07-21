1107abc8c191b330b539b17810627a2d78e5bb4f
Erik Behrends <behrends@users.noreply.github.com>
2020-07-21 14:15:53 +0200

    Remove AndroidViewPager

--- android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.java
+++ android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.java
@@ -9,7 +9,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -37,7 +36,6 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new RNCViewPagerPackage());
       packages.add(new AndroidMapViewPackage());
       return packages;
     }
--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   dependencies: {
-    '@react-native-community/viewpager': {
-      platforms: {
-        android: null,
-        ios: null
-      }
-    },
     'react-native-vector-icons': {
       platforms: {
         android: null,
-        ios: null
-      }
-    }
-  }
+        ios: null,
+      },
+    },
+  },
 };
