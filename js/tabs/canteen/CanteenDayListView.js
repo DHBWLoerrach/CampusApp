253d664227b869fc0b03376089e6110e9c3b3d88
Erik Behrends <behrends@users.noreply.github.com>
2020-09-01 18:00:01 +0200

    Refactoring: rewrite some components as functions

--- js/tabs/canteen/CanteenDayListView.js
+++ js/tabs/canteen/CanteenDayListView.js
@@ -5,62 +5,56 @@ import Colors from '../../util/Colors';
 import { roles } from '../../util/Constants';
 import ListCellTouchable from '../../util/ListCellTouchable';
 
-class MealRow extends Component {
-  render() {
-    const { meal, role } = this.props;
-    let vegetarian;
-    if (meal.vegetarian) {
-      vegetarian = (
-        <Image
-          style={styles.vegetarian}
-          source={require('./img/vegetarian.png')}
-        />
-      );
-    }
-    let price = meal.prices[0].price; // Student
-    if (role === roles[3] || role === roles[1]) {
-      // Gäste oder Lehrbeauftragte
-      price = meal.prices[2].price;
-    } else if (role === roles[2]) {
-      // Mitarbeiter
-      price = meal.prices[1].price;
-    }
-    return (
-      <ListCellTouchable
-        underlayColor={Colors.cellBorder}
-        onPress={this.props.onPress}
-      >
-        <View style={styles.row}>
-          <Text style={styles.name}>{meal.name}</Text>
-          <View style={styles.right}>
-            <Text style={styles.price}>{price}</Text>
-            {vegetarian}
-          </View>
-        </View>
-      </ListCellTouchable>
+function MealRow({ meal, role, onPress }) {
+  let vegetarian;
+  if (meal.vegetarian) {
+    vegetarian = (
+      <Image
+        style={styles.vegetarian}
+        source={require('./img/vegetarian.png')}
+      />
     );
   }
+  let price = meal.prices[0].price; // Student
+  if (role === roles[3] || role === roles[1]) {
+    // Gäste oder Lehrbeauftragte
+    price = meal.prices[2].price;
+  } else if (role === roles[2]) {
+    // Mitarbeiter
+    price = meal.prices[1].price;
+  }
+  return (
+    <ListCellTouchable
+      underlayColor={Colors.cellBorder}
+      onPress={onPress}
+    >
+      <View style={styles.row}>
+        <Text style={styles.name}>{meal.name}</Text>
+        <View style={styles.right}>
+          <Text style={styles.price}>{price}</Text>
+          {vegetarian}
+        </View>
+      </View>
+    </ListCellTouchable>
+  );
 }
 
-export default class CanteenDayListView extends Component {
-  _showMealInfo(meal) {
+export default function CanteenDayListView({ meals, role }) {
+  function _showMealInfo(meal) {
     if (meal.addition && Array.isArray(meal.addition)) {
       Alert.alert('Inhaltsstoffe', meal.addition.join('\n'));
     }
   }
 
-  render() {
-    const { meals, role } = this.props;
-    const mealRows = meals.map((meal, index) => (
-      <MealRow
-        key={'meal' + index}
-        meal={meal}
-        role={role}
-        onPress={() => this._showMealInfo(meal)}
-      />
-    ));
-    return <View>{mealRows}</View>;
-  }
+  const mealRows = meals.map((meal, index) => (
+    <MealRow
+      key={'meal' + index}
+      meal={meal}
+      role={role}
+      onPress={() => _showMealInfo(meal)}
+    />
+  ));
+  return <View>{mealRows}</View>;
 }
 
 const styles = StyleSheet.create({
