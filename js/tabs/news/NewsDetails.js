d8ca332eae45547cb91b3252cfc59c6a8086a043
Erik Behrends <behrends@users.noreply.github.com>
2020-10-15 19:24:40 +0200

    News/Android: PDF links are routed through Google Docs Viewer

--- js/tabs/news/NewsDetails.js
+++ js/tabs/news/NewsDetails.js
@@ -9,6 +9,7 @@ import { DHBW_EVENTS } from '../../util/fetcher/FetchManager';
 
 export default ({ route }) => {
   const fontSize = 'font-size: 42px;';
+  const googleDocsPDF = "https://docs.google.com/gview?embedded=true&url="
   let {
     heading,
     subheading,
@@ -30,15 +31,22 @@ export default ({ route }) => {
   body = body.replace(/font-size:/g, 'fs');
   // Remove target="_blank" in URLs (otherwise they won't work on iOS)
   body = body.replace(/target="_blank"/g, '');
+  if(Platform.OS === 'android') {
+    // on Android use embedded Google docs viewer for PDF (WebView won't work)
+    // see https://github.com/facebook/react-native/issues/6488
+    // route all PDF links through Google docs viewer
+    body = body.replace(/(href=")(.+\.pdf)"/g,`href="${googleDocsPDF}$2"`)
+    console.log('XXXX');
+    console.log(body);
+  }
   let attachmentFooter = '';
   if (attachments && attachments.length >= 1) {
     let attachmentsHTML = '';
     attachments.forEach((attachment) => {
       let url = attachment.url;
-      // on Android use embedded Google docs viewer for PDFs (WebView won't work)
-      // see https://github.com/facebook/react-native/issues/6488
+      // route all PDF attachment through Google docs viewer
       if (url.slice(-4) === '.pdf' && Platform.OS === 'android')
-        url = 'http://docs.google.com/gview?embedded=true&url=' + url;
+        url = googleDocsPDF + url;
       attachmentsHTML += `${attachment.title} <br/> 
         <a href='${url}'>Herunterladen (${attachment.size})</a> 
         <br/>
