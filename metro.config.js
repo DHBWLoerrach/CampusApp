52e43a44639355c36d82d4d5d94b4a48c1fe7627
Erik Behrends <behrends@users.noreply.github.com>
2021-03-29 14:20:59 +0200

    Upgrade React Native to v0.64

--- metro.config.js
+++ metro.config.js
@@ -4,25 +4,27 @@
  *
  * @format
  */
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts, assetExts }
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
     transformer: {
       getTransformOptions: async () => ({
         transform: {
           experimentalImportSupport: false,
-          inlineRequires: false
-        }
+          inlineRequires: true,
+        },
       }),
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
+      babelTransformerPath: require.resolve(
+        'react-native-svg-transformer'
+      ),
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
   };
 })();
