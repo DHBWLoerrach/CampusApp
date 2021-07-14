320dfd922263188a74c0289a40ba8276079d6a22
Erik Behrends <behrends@users.noreply.github.com>
2021-07-14 15:58:07 +0200

    Rewrite component as function

--- js/drawer-screens/dualis/DualisStatistics.js
+++ js/drawer-screens/dualis/DualisStatistics.js
@@ -1,36 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ActivityIndicator from '../../util/DHBWActivityIndicator';
 import AsyncStorage from '@react-native-community/async-storage';
 import Colors from '../../util/Colors';
 
-class DualisStatistics extends React.Component {
-  constructor(props) {
-    super(props);
+export default function DualisStatistics({ route, navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [noContent, setNoContent] = useState(false);
+  const [better, setBetter] = useState(null);
+  const [equal, setEqual] = useState(null);
+  const [worse, setWorse] = useState(null);
+  const [failureRate, setFailureRate] = useState(null);
+  const [error, setError] = useState(null);
 
-    this.state = {
-      loading: false,
-      loginFailed: false,
-      noContent: false,
-      better: null,
-      equal: null,
-      worse: null,
-      failureRate: null,
-      error: null,
-    };
-
-    this.load = this.load.bind(this);
-  }
-
-  componentDidMount() {
-    this.focusListener = this.props.navigation.addListener(
-      'focus',
-      () => this.load(this.props.route.params.id)
+  useEffect(() => {
+    focusListener = navigation.addListener('focus', () =>
+      load(route.params.id)
     );
-  }
+  }, []);
 
-  async load(enrollmentId) {
-    this.setState({ loading: true });
+  async function load(enrollmentId) {
+    setLoading(true);
 
     const token = await AsyncStorage.getItem('dualisToken');
 
@@ -49,126 +39,108 @@ class DualisStatistics extends React.Component {
         }
       ).then((response) => {
         if (response.status == 204) {
-          this.setState({ noContent: true, error: null });
+          setNoContent(true);
+          setError(null);
         }
 
         response.json().then((json) => {
           if (response.status == 500) {
             if (json.message != 'Invalid token') {
-              this.setState({ error: json.message, noContent: true });
+              setError(json.message);
+              setNoContent(true);
             } else {
-              this.props.navigation.navigate('DualisLogin');
+              navigation.navigate('DualisLogin');
             }
           }
 
           if (response.status == 200) {
-            this.setState({
-              better: json.better.replace('.', ','),
-              equal: json.equal.replace('.', ','),
-              worse: json.worse.replace('.', ','),
-              failureRate: json.failureRate.replace('.', ','),
-              noContent: false,
-              error: null,
-            });
+            setBetter(json.better.replace('.', ','));
+            setEqual(json.equal.replace('.', ','));
+            setWorse(json.worse.replace('.', ','));
+            setFailureRate(json.failureRate.replace('.', ','));
+            setNoContent(false);
+            setError(null);
           }
         });
-
-        this.setState({ loading: false });
       });
     } catch (ex) {
-      this.setState({ error: ex, loading: false });
+      setError(ex);
     }
+    setLoading(false);
   }
 
-  render() {
-    if (this.state.loading) {
-      return (
-        <View style={styles.center}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
-    if (this.state.noContent) {
-      return (
-        <View style={styles.center}>
-          <Text style={styles.message}>
-            Die Statistiken zu diesem Modul können nicht erfasst
-            werden!
-          </Text>
-        </View>
-      );
-    }
-
-    if (this.state.error) {
-      return (
-        <View style={styles.center}>
-          <Text style={styles.message}>{this.state.error}</Text>
-        </View>
-      );
-    }
-
+  if (loading) {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={{ fontSize: 22, textAlign: 'center' }}>
-            {this.props.route.params.name}
-          </Text>
-          <View style={styles.percentageBlock}>
-            <Text style={styles.text}>
-              Besser als Du abgeschnitten haben...
-            </Text>
-            <Text style={styles.percentage}>
-              {this.state.better}%
-            </Text>
-          </View>
-
-          <View style={styles.percentageBlock}>
-            <Text style={styles.text}>
-              Gleich gut wie Du abgeschnitten haben...
-            </Text>
-            <Text style={styles.percentage}>{this.state.equal}%</Text>
-          </View>
-
-          <View style={styles.percentageBlock}>
-            <Text style={styles.text}>
-              Schlechter als Du abgeschnitten haben...
-            </Text>
-            <Text style={styles.percentage}>{this.state.worse}%</Text>
-          </View>
-
-          <View style={styles.percentageBlock}>
-            <Text style={styles.text}>
-              ...der Studenten Deines Kurses.*
-            </Text>
-          </View>
-
-          <View style={styles.percentageBlock}>
-            <Text style={styles.text}>Durchfallquote:</Text>
-            <Text style={styles.percentage}>
-              {this.state.failureRate}%
-            </Text>
-          </View>
-        </View>
-        <View>
-          <Text
-            style={{
-              fontSize: 10,
-              paddingBottom: 5,
-              textAlign: 'center',
-            }}
-          >
-            * bei kursübergreifenden Modulen wirst Du natürlich nur
-            mit Studenten verglichen, die dieses Modul ebenfalls
-            gewählt haben.
-          </Text>
-        </View>
+      <View style={styles.center}>
+        <ActivityIndicator />
       </View>
     );
   }
-}
 
-export default DualisStatistics;
+  if (noContent) {
+    return (
+      <View style={styles.center}>
+        <Text style={styles.message}>
+          Die Statistiken zu diesem Modul können nicht erfasst werden!
+        </Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text style={styles.message}>{error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.name}>{route.params.name}</Text>
+        <View style={styles.percentageBlock}>
+          <Text style={styles.text}>
+            Besser als Du abgeschnitten haben...
+          </Text>
+          <Text style={styles.percentage}>{better}%</Text>
+        </View>
+
+        <View style={styles.percentageBlock}>
+          <Text style={styles.text}>
+            Gleich gut wie Du abgeschnitten haben...
+          </Text>
+          <Text style={styles.percentage}>{equal}%</Text>
+        </View>
+
+        <View style={styles.percentageBlock}>
+          <Text style={styles.text}>
+            Schlechter als Du abgeschnitten haben...
+          </Text>
+          <Text style={styles.percentage}>{worse}%</Text>
+        </View>
+
+        <View style={styles.percentageBlock}>
+          <Text style={styles.text}>
+            ...der Studenten Deines Kurses.*
+          </Text>
+        </View>
+
+        <View style={styles.percentageBlock}>
+          <Text style={styles.text}>Durchfallquote:</Text>
+          <Text style={styles.percentage}>{failureRate}%</Text>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.note}>
+          * bei kursübergreifenden Modulen wirst Du natürlich nur mit
+          Studenten verglichen, die dieses Modul ebenfalls gewählt
+          haben.
+        </Text>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -182,6 +154,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  name: {
+    fontSize: 22,
+    textAlign: 'center',
   },
   message: {
     fontSize: 18,
@@ -203,5 +179,10 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     marginHorizontal: 20,
+  },
+  note: {
+    fontSize: 10,
+    paddingBottom: 5,
+    textAlign: 'center',
   },
 });
