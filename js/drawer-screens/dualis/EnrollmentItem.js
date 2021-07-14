5a6dcda95577530e0a8ad0499fc80bdd6d0bd878
Erik Behrends <behrends@users.noreply.github.com>
2021-07-14 16:01:23 +0200

    Rewrite component as function

--- js/drawer-screens/dualis/EnrollmentItem.js
+++ js/drawer-screens/dualis/EnrollmentItem.js
@@ -8,66 +8,47 @@ import {
 import Colors from '../../util/Colors';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-class EnrollmentItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View>
-          <Text style={{ fontSize: 20 }}>
-            {this.props.enrollment.moduleResult[0].name}
-          </Text>
-          <Text>
-            Modulnummer:{' '}
-            {this.props.enrollment.moduleResult[0].number}
-          </Text>
-          <Text>Note: {this.props.enrollment.grade}</Text>
-          <Text>
-            Credits: {this.props.enrollment.moduleResult[0].credits}
-          </Text>
-          <Text>Semester: {this.props.enrollment.semester}</Text>
-          <Text>Status: {this.props.enrollment.status}</Text>
-        </View>
-        <View style={styles.iconBar}>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate('DualisDetail', {
-                details:
-                  this.props.enrollment.moduleResult[0]
-                    .lectureResults,
-              });
-            }}
-          >
-            <Icon
-              name="book-search"
-              color={Colors.dhbwRed}
-              size={50}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate('DualisStatistics', {
-                id: this.props.enrollment.id,
-                name: this.props.enrollment.moduleResult[0].name,
-              });
-            }}
-          >
-            <Icon
-              name="chart-areaspline"
-              color={Colors.dhbwRed}
-              size={50}
-            />
-          </TouchableOpacity>
-        </View>
+export default function EnrollmentItem({ navigation, enrollment }) {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.name}>
+          {enrollment.moduleResult[0].name}
+        </Text>
+        <Text>Modulnummer: {enrollment.moduleResult[0].number}</Text>
+        <Text>Note: {enrollment.grade}</Text>
+        <Text>Credits: {enrollment.moduleResult[0].credits}</Text>
+        <Text>Semester: {enrollment.semester}</Text>
+        <Text>Status: {enrollment.status}</Text>
       </View>
-    );
-  }
+      <View style={styles.iconBar}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('DualisDetail', {
+              details: enrollment.moduleResult[0].lectureResults,
+            });
+          }}
+        >
+          <Icon name="book-search" color={Colors.dhbwRed} size={50} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('DualisStatistics', {
+              id: enrollment.id,
+              name: enrollment.moduleResult[0].name,
+            });
+          }}
+        >
+          <Icon
+            name="chart-areaspline"
+            color={Colors.dhbwRed}
+            size={50}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
-
-export default EnrollmentItem;
 
 const styles = StyleSheet.create({
   container: {
@@ -79,6 +60,9 @@ const styles = StyleSheet.create({
     marginTop: 20,
     padding: 10,
     borderRadius: 10,
+  },
+  name: {
+    fontSize: 20,
   },
   iconBar: {
     justifyContent: 'space-between',
