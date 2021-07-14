8dc878c02c58e6f999e09d9e9d9187d8f9d4d7a5
Erik Behrends <behrends@users.noreply.github.com>
2021-07-14 16:08:00 +0200

    Rewrite component as function

--- js/drawer-screens/dualis/DualisNavigator.js
+++ js/drawer-screens/dualis/DualisNavigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Platform, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,36 +13,25 @@ import DualisMain from './DualisMain';
 import DualisDetail from './DualisDetail';
 import DualisStatistics from './DualisStatistics';
 
-class DualisNavigator extends React.Component {
-  constructor(props) {
-    super(props);
+export default function DualisNavigator({ navigation }) {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [intro, setIntro] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    this.state = {
-      authenticated: false,
-      intro: false,
-      loading: false,
-    };
-
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-  }
-
-  componentDidMount() {
-    this.focusListener = this.props.navigation.addListener(
-      'focus',
-      () => this.isAuthenticated()
+  useEffect(() => {
+    focusListener = navigation.addListener('focus', () =>
+      isAuthenticated()
     );
-  }
+  }, []);
 
-  async isAuthenticated() {
-    this.setState({ loading: true });
+  async function isAuthenticated() {
+    setLoading(true);
     const token = await AsyncStorage.getItem('dualisToken');
 
     if (token == null) {
-      this.setState({
-        authenticated: false,
-        intro: true,
-        loading: false,
-      });
+      setAuthenticated(false);
+      setIntro(true);
+      setLoading(false);
       return;
     }
 
@@ -50,161 +39,153 @@ class DualisNavigator extends React.Component {
     try {
       tokenDecoded = jwt_decode(token);
       if (Date.now() >= tokenDecoded.standardclaims.exp * 1000) {
-        this.setState({
-          authenticated: false,
-          intro: false,
-          loading: false,
-        });
+        setAuthenticated(false);
+        setIntro(false);
+        setLoading(false);
         return;
       }
     } catch (err) {
-      this.setState({
-        authenticated: false,
-        intro: false,
-        loading: false,
-      });
+      setAuthenticated(false);
+      setIntro(false);
+      setLoading(false);
       return;
     }
-    this.setState({
-      authenticated: true,
-      intro: false,
-      loading: false,
-    });
-    return;
+    setAuthenticated(true);
+    setIntro(true);
+    setLoading(false);
   }
 
-  render() {
-    const stackHeaderConfig = {
-      headerBackTitle: 'Zurück',
-      headerTintColor: 'white',
-      headerLeft: () => (
-        <MaterialIcon
-          style={{ paddingLeft: 10 }}
-          onPress={() => {
-            this.props.navigation.toggleDrawer();
-          }}
-          name="menu"
-          size={30}
-        />
-      ),
-      headerStyle: {
-        backgroundColor: Colors.dhbwRed,
-        shadowColor: 'transparent', // prevent line below header in iOS
-        ...Platform.select({
-          android: {
-            elevation: 0,
-          },
-        }),
-      },
-    };
+  const stackHeaderConfig = {
+    headerBackTitle: 'Zurück',
+    headerTintColor: 'white',
+    headerLeft: () => (
+      <MaterialIcon
+        style={styles.icon}
+        onPress={() => {
+          navigation.toggleDrawer();
+        }}
+        name="menu"
+        size={30}
+      />
+    ),
+    headerStyle: {
+      backgroundColor: Colors.dhbwRed,
+      shadowColor: 'transparent', // prevent line below header in iOS
+      ...Platform.select({
+        android: {
+          elevation: 0,
+        },
+      }),
+    },
+  };
 
-    const Stack = createStackNavigator();
+  const Stack = createStackNavigator();
 
-    if (this.state.loading) {
-      return (
-        <View style={styles.center}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
+  if (loading) {
     return (
-      <NavigationContainer independent={true}>
-        {this.state.authenticated ? (
-          <Stack.Navigator
-            initialRouteName="DualisMain"
-            screenOptions={stackHeaderConfig}
-          >
-            <Stack.Screen
-              name="DualisMain"
-              component={DualisMain}
-              options={{ headerTitle: 'Dualis' }}
-            />
-            <Stack.Screen
-              name="DualisDetail"
-              component={DualisDetail}
-              options={{ title: 'Dualis Vorlesungsdetails' }}
-            />
-            <Stack.Screen
-              name="DualisStatistics"
-              component={DualisStatistics}
-              options={{ title: 'Dualis Modulstatistik' }}
-            />
-            <Stack.Screen
-              name="DualisLogin"
-              component={DualisLogin}
-              options={{ title: 'Dualis Login' }}
-            />
-          </Stack.Navigator>
-        ) : this.state.intro ? (
-          <Stack.Navigator
-            initialRouteName="DualisIntro"
-            screenOptions={stackHeaderConfig}
-          >
-            <Stack.Screen
-              name="DualisIntro"
-              component={DualisIntro}
-              options={{ title: 'Campus App Dualis' }}
-            />
-            <Stack.Screen
-              name="DualisLogin"
-              component={DualisLogin}
-              options={{ title: 'Dualis Login' }}
-            />
-            <Stack.Screen
-              name="DualisMain"
-              component={DualisMain}
-              options={{ headerTitle: 'Dualis' }}
-            />
-            <Stack.Screen
-              name="DualisDetail"
-              component={DualisDetail}
-              options={{ title: 'Dualis Vorlesungsdetails' }}
-            />
-            <Stack.Screen
-              name="DualisStatistics"
-              component={DualisStatistics}
-              options={{ title: 'Dualis Modulstatistik' }}
-            />
-          </Stack.Navigator>
-        ) : (
-          <Stack.Navigator
-            initialRouteName="DualisLogin"
-            screenOptions={stackHeaderConfig}
-          >
-            <Stack.Screen
-              name="DualisLogin"
-              component={DualisLogin}
-              options={{ title: 'Dualis Login' }}
-            />
-            <Stack.Screen
-              name="DualisMain"
-              component={DualisMain}
-              options={{ headerTitle: 'Dualis' }}
-            />
-            <Stack.Screen
-              name="DualisDetail"
-              component={DualisDetail}
-              options={{ title: 'Dualis Vorlesungsdetails' }}
-            />
-            <Stack.Screen
-              name="DualisStatistics"
-              component={DualisStatistics}
-              options={{ title: 'Dualis Modulstatistik' }}
-            />
-          </Stack.Navigator>
-        )}
-      </NavigationContainer>
+      <View style={styles.center}>
+        <ActivityIndicator />
+      </View>
     );
   }
-}
 
-export default DualisNavigator;
+  return (
+    <NavigationContainer independent={true}>
+      {authenticated ? (
+        <Stack.Navigator
+          initialRouteName="DualisMain"
+          screenOptions={stackHeaderConfig}
+        >
+          <Stack.Screen
+            name="DualisMain"
+            component={DualisMain}
+            options={{ headerTitle: 'Dualis' }}
+          />
+          <Stack.Screen
+            name="DualisDetail"
+            component={DualisDetail}
+            options={{ title: 'Dualis Vorlesungsdetails' }}
+          />
+          <Stack.Screen
+            name="DualisStatistics"
+            component={DualisStatistics}
+            options={{ title: 'Dualis Modulstatistik' }}
+          />
+          <Stack.Screen
+            name="DualisLogin"
+            component={DualisLogin}
+            options={{ title: 'Dualis Login' }}
+          />
+        </Stack.Navigator>
+      ) : intro ? (
+        <Stack.Navigator
+          initialRouteName="DualisIntro"
+          screenOptions={stackHeaderConfig}
+        >
+          <Stack.Screen
+            name="DualisIntro"
+            component={DualisIntro}
+            options={{ title: 'Campus App Dualis' }}
+          />
+          <Stack.Screen
+            name="DualisLogin"
+            component={DualisLogin}
+            options={{ title: 'Dualis Login' }}
+          />
+          <Stack.Screen
+            name="DualisMain"
+            component={DualisMain}
+            options={{ headerTitle: 'Dualis' }}
+          />
+          <Stack.Screen
+            name="DualisDetail"
+            component={DualisDetail}
+            options={{ title: 'Dualis Vorlesungsdetails' }}
+          />
+          <Stack.Screen
+            name="DualisStatistics"
+            component={DualisStatistics}
+            options={{ title: 'Dualis Modulstatistik' }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator
+          initialRouteName="DualisLogin"
+          screenOptions={stackHeaderConfig}
+        >
+          <Stack.Screen
+            name="DualisLogin"
+            component={DualisLogin}
+            options={{ title: 'Dualis Login' }}
+          />
+          <Stack.Screen
+            name="DualisMain"
+            component={DualisMain}
+            options={{ headerTitle: 'Dualis' }}
+          />
+          <Stack.Screen
+            name="DualisDetail"
+            component={DualisDetail}
+            options={{ title: 'Dualis Vorlesungsdetails' }}
+          />
+          <Stack.Screen
+            name="DualisStatistics"
+            component={DualisStatistics}
+            options={{ title: 'Dualis Modulstatistik' }}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   center: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  icon: {
+    paddingLeft: 10,
   },
 });
