43068591a4152a317ec060df656403f48c5f4c07
Erik Behrends <behrends@users.noreply.github.com>
2021-11-19 10:27:24 +0100

    BackgroundFetch: add onTimeout callback in config

--- js/util/notifications/NotificationTaskScheduler.js
+++ js/util/notifications/NotificationTaskScheduler.js
@@ -2,6 +2,19 @@ import BackgroundFetch from 'react-native-background-fetch';
 import NotificationTask from './NotificationTask';
 
 export default function () {
+  // BackgroundFetch event handler.
+  const onEvent = async (taskId) => {
+    await NotificationTask();
+    BackgroundFetch.finish(taskId);
+  }
+
+  // Timeout callback is executed when your Task has exceeded its allowed running-time.
+  // You must stop what you're doing immediately BackgroundFetch.finish(taskId)
+  const onTimeout = async (taskId) => {
+    console.warn('[BackgroundFetch] TIMEOUT task: ', taskId);
+    BackgroundFetch.finish(taskId);
+  }
+
   BackgroundFetch.configure(
     {
       minimumFetchInterval: 24 * 60, // once a day = 24 * 60 minutes
@@ -10,9 +23,7 @@ export default function () {
       requiredNetworkType: BackgroundFetch.NETWORK_TYPE_UNMETERED,
       enableHeadless: true,
     },
-    async (taskId) => {
-      await NotificationTask();
-      BackgroundFetch.finish(taskId);
-    }
+    onEvent,
+    onTimeout
   );
 }
