4ae6ce6fe305e7bc92f7316f24bd9f5e0c0e6883
Louis König <kingdev.louis@gmail.com>
2021-11-20 11:46:35 +0100

    reverted that env.example was unintentionally deleted in last commit

---
+++ env.example.js
@@ -0,0 +1,9 @@
+// @flow
+'use strict';
+
+module.exports = {
+  courseList: [], // list of course names
+  canteenApiKey: '', // api-key for SWFR canteen (swfr.de),
+  fbAccessToken: '', // appId|appSecret for facebook feeds
+  enableStuV: false
+};
