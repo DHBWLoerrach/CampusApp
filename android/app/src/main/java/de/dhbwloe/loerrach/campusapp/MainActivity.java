5db49bd141574c806136965dd768067dafdcdb4f
Erik Behrends <behrends@users.noreply.github.com>
2022-04-29 11:56:37 +0200

    Upgrade React Native to version 0.68.1

--- android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainActivity.java
+++ android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainActivity.java
@@ -9,7 +9,6 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.google.android.material.snackbar.Snackbar;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -19,7 +18,7 @@ import de.dhbwloe.loerrach.campusapp.nfcreader.NfcBadgeListener;
 
 public class MainActivity extends ReactActivity {
 
-    private NfcBadgeListener nfcBadgeListener;
+    // private NfcBadgeListener nfcBadgeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,47 +26,47 @@ public class MainActivity extends ReactActivity {
       DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
       dfs.setDecimalSeparator(',');
       final DecimalFormat euroFormat = new DecimalFormat("0.00€", dfs);
-      nfcBadgeListener = new NfcBadgeListener(this);
-      nfcBadgeListener.registerNfcBadgeInterface(new NfcBadgeInterface() {
-            @Override
-            public void onNfcReaderStateChanged(boolean state) {}
+    //   nfcBadgeListener = new NfcBadgeListener(this);
+    //   nfcBadgeListener.registerNfcBadgeInterface(new NfcBadgeInterface() {
+    //         @Override
+    //         public void onNfcReaderStateChanged(boolean state) {}
 
-            @Override
-            public void onNfcReaderReceived(int balance, int lastTransaction) {
-                double badgeBalance = balance / 100.0;
-                double lastTransactionValue = -lastTransaction / 100.0;
-                String msg = "Guthaben: " + euroFormat.format(badgeBalance) + "\n"
-                        + "Letzte Transaktion: " + euroFormat.format(lastTransactionValue);
-                Snackbar snackbar =
-                        Snackbar.make(findViewById(android.R.id.content).getRootView(), msg, Snackbar.LENGTH_LONG);
-                TextView snackbarTextView =
-                        snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
-                snackbarTextView.setTextSize(24);
-                snackbar.show();
-            }
-        });
-      nfcBadgeListener.startNfcListener();
+    //         @Override
+    //         public void onNfcReaderReceived(int balance, int lastTransaction) {
+    //             double badgeBalance = balance / 100.0;
+    //             double lastTransactionValue = -lastTransaction / 100.0;
+    //             String msg = "Guthaben: " + euroFormat.format(badgeBalance) + "\n"
+    //                     + "Letzte Transaktion: " + euroFormat.format(lastTransactionValue);
+    //             Snackbar snackbar =
+    //                     Snackbar.make(findViewById(android.R.id.content).getRootView(), msg, Snackbar.LENGTH_LONG);
+    //             TextView snackbarTextView =
+    //                     snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+    //             snackbarTextView.setTextSize(24);
+    //             snackbar.show();
+    //         }
+    //     });
+    //   nfcBadgeListener.startNfcListener();
     }
 
     /* nfc listener related callbacks */
     @Override
     public void onResume() {
         super.onResume();
-        nfcBadgeListener.resumeForefrontDispatcher();
+        // nfcBadgeListener.resumeForefrontDispatcher();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        nfcBadgeListener.pauseForefrontDispatcher();
+        // nfcBadgeListener.pauseForefrontDispatcher();
     }
 
     @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
-            nfcBadgeListener.handleNfcEvent(intent);
-        }
+        // if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+        //     nfcBadgeListener.handleNfcEvent(intent);
+        // }
     }
 
     /**
@@ -79,13 +78,26 @@ public class MainActivity extends ReactActivity {
         return "CampusApp";
     }
 
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+     * you can specify the rendered you wish to use (Fabric or the older renderer).
+    */
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
+        return new MainActivityDelegate(this, getMainComponentName());
+    }
+
+    public static class MainActivityDelegate extends ReactActivityDelegate {
+        public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+            super(activity, mainComponentName);
+        }
+
+        @Override
+        protected ReactRootView createRootView() {
+            ReactRootView reactRootView = new ReactRootView(getContext());
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+            return reactRootView;
+        }
     }
 }
--- android/app/src/main/java/de/dhbwloe/loerrach/campusapp/nfcreader/NfcBadgeListener.java
+++ android/app/src/main/java/de/dhbwloe/loerrach/campusapp/nfcreader/NfcBadgeListener.java
@@ -63,8 +63,8 @@ public class NfcBadgeListener {
         isRunning = true;
 
         oAdapter = NfcAdapter.getDefaultAdapter(mainActivity);
-
-        oPendingIntent = PendingIntent.getActivity(mainActivity, 0, new Intent(mainActivity, mainActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+ 
+        oPendingIntent = PendingIntent.getActivity(mainActivity, 0, new Intent(mainActivity, mainActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | PendingIntent.FLAG_IMMUTABLE), 0);
         IntentFilter techDiscovered = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
         aFilters = new IntentFilter[]{techDiscovered};
         aTechLists = new String[][]{new String[]{IsoDep.class.getName()}};
