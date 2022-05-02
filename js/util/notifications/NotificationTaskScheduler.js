bbe4ef9a75f154e5c670179c7882ceeaf1812370
Erik Behrends <behrends@users.noreply.github.com>
2022-05-02 18:21:34 +0200

    Android: register headless background task

--- js/util/notifications/NotificationTaskScheduler.js
+++ js/util/notifications/NotificationTaskScheduler.js
@@ -6,19 +6,23 @@ export default function () {
   const onEvent = async (taskId) => {
     await NotificationTask();
     BackgroundFetch.finish(taskId);
-  }
+  };
 
   // Timeout callback is executed when your Task has exceeded its allowed running-time.
   // You must stop what you're doing immediately BackgroundFetch.finish(taskId)
   const onTimeout = async (taskId) => {
     console.warn('[BackgroundFetch] TIMEOUT task: ', taskId);
     BackgroundFetch.finish(taskId);
-  }
+  };
 
   BackgroundFetch.configure(
     {
       minimumFetchInterval: 24 * 60, // once a day = 24 * 60 minutes
       requiredNetworkType: BackgroundFetch.NETWORK_TYPE_UNMETERED,
+      // Android options
+      startOnBoot: true,
+      stopOnTerminate: false,
+      enableHeadless: true, // handling fetch events after app termination.
     },
     onEvent,
     onTimeout
