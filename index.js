b7941fa9422c960767c777e9b66f03f378edf8e6
Erik Behrends <behrends@users.noreply.github.com>
2022-05-02 17:45:40 +0200

    Android: run headless background fetch for notifications

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import 'react-native-gesture-handler';
 import { AppRegistry, Platform } from 'react-native';
 import PushNotification from 'react-native-push-notification';
+import BackgroundFetch from 'react-native-background-fetch';
 import CampusApp from './js/CampusApp';
+import NotificationTask from './js/util/notifications/NotificationTask';
 
 PushNotification.configure({
   requestPermissions: Platform.OS === 'ios',
@@ -11,3 +13,24 @@ PushNotification.createChannel({
   channelName: `DHBW Channel`, // required on Android
 });
 AppRegistry.registerComponent('CampusApp', () => CampusApp);
+
+/// BackgroundFetch Android Headless Event Receiver.
+/// Called when the Android app is terminated.
+if (Platform.OS === 'android') {
+  const backgroundFetchHeadlessTask = async (event) => {
+    if (event.timeout) {
+      BackgroundFetch.finish(event.taskId);
+      return;
+    }
+
+    await NotificationTask();
+
+    // Required:  Signal to native code that your task is complete.
+    // If you don't do this, your app could be terminated and/or assigned
+    // battery-blame for consuming too much time in background.
+    BackgroundFetch.finish(event.taskId);
+  };
+
+  /// Register headless BackgroundFetch handler.
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
