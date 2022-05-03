1081f4bcc94cbc38883f0151fd613a52f77fdfa5
Erik Behrends <behrends@users.noreply.github.com>
2022-05-03 12:00:42 +0200

    Revert removal of ICalFetcher

--- js/util/fetcher/FetchManager.js
+++ js/util/fetcher/FetchManager.js
@@ -1,6 +1,8 @@
 import RSSFetcher from './RSSFetcher';
 import CacheFetcher from './CacheFetcher';
+import ICalFetcher from './ICalFetcher';
 
+export const DHBW_COURSE = 'DHBW_COURSE';
 export const DHBW_NEWS = 'DHBW_NEWS';
 export const DHBW_EVENTS = 'DHBW_EVENTS';
 
@@ -20,6 +22,12 @@ class FetchManager {
         'https://dhbw-loerrach.de/rss-campus-app-termine'
       ),
       DHBW_EVENTS
+    );
+
+    this.fetcher[DHBW_COURSE] = new CacheFetcher(
+      new ICalFetcher(),
+      DHBW_COURSE,
+      60
     );
   }
 
