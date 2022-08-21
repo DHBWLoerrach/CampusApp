59116cea563784557a40f4fa80dab08479f8f214
TobiasWeigel3005 <87417148+TobiasWeigel3005@users.noreply.github.com>
2022-08-21 11:19:07 +0200

    Made InfoImage Component fill entire screen

--- js/tabs/service/InfoImage.js
+++ js/tabs/service/InfoImage.js
@@ -1,15 +1,17 @@
 import React, {Component, useContext} from 'react';
-import { Image } from 'react-native';
+import {Image, View} from 'react-native';
 import Styles from '../../Styles/StyleSheet';
 import {ColorSchemeContext} from "../../context/ColorSchemeContext";
 
 export default function InfoImage(){
   const colorContext = useContext(ColorSchemeContext);
     return (
-      <Image
-        style={[Styles.InfoImage.img, {backgroundColor: colorContext.colorScheme.background}]}
-        resizeMode="contain"
-        source={require('./img/CampusHangstrasse.jpg')}
-      />
+        <View style={[Styles.InfoImage.container, {backgroundColor: colorContext.colorScheme.background}]}>
+          <Image
+              style={Styles.InfoImage.img}
+              resizeMode="contain"
+              source={require('./img/CampusHangstrasse.jpg')}
+          />
+        </View>
     );
 }
