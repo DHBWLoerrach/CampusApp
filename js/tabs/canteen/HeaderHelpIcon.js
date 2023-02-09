90417bed9784a6f3e246efbfe0d556608226902a
Erik Behrends <behrends@users.noreply.github.com>
2023-02-09 11:16:57 +0100

    Canteen: Update text/info in Alert

--- js/tabs/canteen/HeaderHelpIcon.js
+++ js/tabs/canteen/HeaderHelpIcon.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Alert, Platform } from 'react-native';
+import { Alert } from 'react-native';
 import HeaderIcon from '../../util/HeaderIcon';
 
 const textCanteenInfo =
-  'Mo-Fr geöffnet 8.30-13.45 Uhr \n' +
+  'Mo-Fr geöffnet 9.30-13.45 Uhr \n' +
   'Mittagessen: 11.45-13.30 Uhr\n\n' +
   'Die Preise werden für die von Dir gewählte Personengruppe ' +
-  'angezeigt (siehe Services > Einstellungen).\n\n' +
-  'Tippe auf ein Gericht, um Informationen über Inhaltsstoffe anzeigen zu lassen.';
+  'angezeigt (siehe Services > Einstellungen).';
 
 export default () => (
   <HeaderIcon
