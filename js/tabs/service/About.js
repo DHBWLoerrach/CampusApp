c9913d8eb64530c306a498fa3c5991da9cb8c780
Erik Behrends <behrends@users.noreply.github.com>
2023-03-21 18:38:52 +0100

    Bump version to 2.8.0

--- js/tabs/service/About.js
+++ js/tabs/service/About.js
@@ -55,7 +55,7 @@ export default function About() {
           { color: colorContext.colorScheme.text },
         ]}
       >
-        Version (App): 2.7.1
+        Version (App): 2.8.0
       </Text>
     </ScrollView>
   );
