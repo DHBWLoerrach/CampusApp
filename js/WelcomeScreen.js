d915e7908ca106544a7ece3e15303de3f83d367f
Erik Behrends <behrends@users.noreply.github.com>
2023-03-22 13:42:20 +0100

    Format with Prettier

--- js/WelcomeScreen.js
+++ js/WelcomeScreen.js
@@ -1,11 +1,5 @@
-import React, {useContext, useState} from 'react';
-import {
-  Image,
-  ScrollView, StyleSheet,
-  Switch,
-  Text,
-  View,
-} from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Image, ScrollView, Switch, Text, View } from 'react-native';
 
 import {
   TextDisclaimer,
@@ -14,14 +8,8 @@ import {
 import RoleSelection from './tabs/service/RoleSelection';
 import NotificationSettings from './util/NotificationSettings';
 import Styles from './Styles/StyleSheet';
-import UIButton from "./ui/UIButton";
-import {ColorSchemeContext} from "./context/ColorSchemeContext";
-
-//TODO: Check if its necessary
-/*const ButtonTouchable =
-  Platform.OS === 'android'
-    ? TouchableNativeFeedback
-    : TouchableOpacity;*/
+import UIButton from './ui/UIButton';
+import { ColorSchemeContext } from './context/ColorSchemeContext';
 
 export default function WelcomeScreen(props) {
   const [disclaimerChecked, checkDisclaimer] = useState(false);
@@ -35,14 +23,30 @@ export default function WelcomeScreen(props) {
   };
 
   return (
-    <View style={[Styles.WelcomeScreen.container, {backgroundColor: colorContext.colorScheme.background}]}>
+    <View
+      style={[
+        Styles.WelcomeScreen.container,
+        { backgroundColor: colorContext.colorScheme.background },
+      ]}
+    >
       <Image
         style={Styles.WelcomeScreen.headerImage}
         source={require('./img/drawer-header.png')}
       />
-      <ScrollView style={[Styles.WelcomeScreen.contentContainer, {backgroundColor: colorContext.colorScheme.background}]}>
+      <ScrollView
+        style={[
+          Styles.WelcomeScreen.contentContainer,
+          { backgroundColor: colorContext.colorScheme.background },
+        ]}
+      >
         <View style={Styles.WelcomeScreen.header}>
-          <Text style={[Styles.WelcomeScreen.heading, Styles.WelcomeScreen.welcome, {color: colorContext.colorScheme.dhbwRed}]}>
+          <Text
+            style={[
+              Styles.WelcomeScreen.heading,
+              Styles.WelcomeScreen.welcome,
+              { color: colorContext.colorScheme.dhbwRed },
+            ]}
+          >
             Willkommen an der DHBW Lörrach
           </Text>
           <Image
@@ -51,14 +55,16 @@ export default function WelcomeScreen(props) {
           />
         </View>
         <View>
-          <Text style={{color: colorContext.colorScheme.text}}>
+          <Text style={{ color: colorContext.colorScheme.text }}>
             Diese App ermöglicht den mobilen Zugriff auf News für
             Studierende, Vorlesungspläne, Speiseplan der Mensa…
           </Text>
         </View>
         <View style={Styles.WelcomeScreen.selection}>
           <View>
-            <Text style={{color: colorContext.colorScheme.text}}>{textPersonCategory}</Text>
+            <Text style={{ color: colorContext.colorScheme.text }}>
+              {textPersonCategory}
+            </Text>
           </View>
           <RoleSelection
             role={role}
@@ -67,7 +73,7 @@ export default function WelcomeScreen(props) {
         </View>
 
         <View style={Styles.WelcomeScreen.notificationSettings}>
-          <Text style={{color: colorContext.colorScheme.text}}>
+          <Text style={{ color: colorContext.colorScheme.text }}>
             Hier kannst Du auswählen, welche Benachrichtigungen Du
             erhalten möchtest. Dies geschieht höchstens einmal am Tag.
           </Text>
@@ -80,39 +86,27 @@ export default function WelcomeScreen(props) {
         </View>
 
         <View style={Styles.WelcomeScreen.toggleContainer}>
-          <Text style={{color: colorContext.colorScheme.text}}>Ich stimme zu</Text>
+          <Text style={{ color: colorContext.colorScheme.text }}>
+            Ich stimme zu
+          </Text>
           <Switch
-              trackColor={{false: colorContext.colorScheme.dhbwGray, true: colorContext.colorScheme.dhbwRed}}
-              thumbColor="#f4f3f4"
-              onValueChange={(value) => checkDisclaimer(value)}
-              value={disclaimerChecked}
+            trackColor={{
+              false: colorContext.colorScheme.dhbwGray,
+              true: colorContext.colorScheme.dhbwRed,
+            }}
+            thumbColor="#f4f3f4"
+            onValueChange={(value) => checkDisclaimer(value)}
+            value={disclaimerChecked}
           />
         </View>
         <View style={Styles.WelcomeScreen.footer}>
-          {/*<View style={Styles.WelcomeScreen.agreeDisclaimer}>
-            <Text style={Styles.WelcomeScreen.disclaimerLabel}>Ich stimme zu</Text>
-            <Switch
-              onValueChange={(value) => checkDisclaimer(value)}
-              value={disclaimerChecked}
-            />
-          </View>*/}
-
-          {/*<ButtonTouchable onPress={_onSubmit}>
-            <Text
-              style={[
-                Styles.WelcomeScreen.submit,
-                {
-                  color:
-                    disclaimerChecked && role
-                      ? Colors.dhbwRed
-                      : Colors.dhbwGray,
-                },
-              ]}
-            >
-              {'Start >'}
-            </Text>
-          </ButtonTouchable>*/}
-          <UIButton size={"small"} onClick={onSubmit} disabled={! (disclaimerChecked && role)}>Start</UIButton>
+          <UIButton
+            size="small"
+            onClick={onSubmit}
+            disabled={!(disclaimerChecked && role)}
+          >
+            Start
+          </UIButton>
         </View>
       </ScrollView>
     </View>
