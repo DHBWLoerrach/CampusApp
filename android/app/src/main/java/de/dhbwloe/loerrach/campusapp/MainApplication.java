1a74088f50c21ba07940d9e5824e193d4075ff66
Erik Behrends <behrends@users.noreply.github.com>
2023-08-11 14:29:45 +0200

    Android: Remove map used in StuV

--- android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.java
+++ android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
-import de.dhbwloe.loerrach.campusapp.mapview.AndroidMapViewPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
@@ -33,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new AndroidMapViewPackage());
+      // packages.add(new MyPackage());
       return packages;
     }
 
