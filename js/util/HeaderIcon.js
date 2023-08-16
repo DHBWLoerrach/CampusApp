d3d6902d667dfa4a3fd418a9b6698ebe443588e2
Erik Behrends <behrends@users.noreply.github.com>
2023-08-16 17:04:14 +0200

    Cleanup imports

--- js/util/HeaderIcon.js
+++ js/util/HeaderIcon.js
@@ -1,6 +1,4 @@
-import React, {useContext} from 'react';
 import {
-  StyleSheet,
   Platform,
   TouchableOpacity,
   TouchableNativeFeedback,
@@ -8,7 +6,6 @@ import {
 } from 'react-native';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import Styles from '../Styles/StyleSheet';
-import {ColorSchemeContext} from "../context/ColorSchemeContext";
 
 const Touchable =
   Platform.OS === 'ios' ? TouchableOpacity : TouchableNativeFeedback;
