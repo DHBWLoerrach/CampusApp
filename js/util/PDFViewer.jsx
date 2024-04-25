afa756de9605269c5e8b50b24d182ab97b9f5d21
Erik Behrends <behrends@users.noreply.github.com>
2024-04-25 12:02:17 +0200

    make sure PDF is reloaded when screen is focused

--- js/util/PDFViewer.jsx
+++ js/util/PDFViewer.jsx
@@ -1,7 +1,19 @@
+import { useCallback, useRef } from 'react';
 import { Platform } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
 
 export default function PDFViewer({ source }) {
+  const webViewRef = useRef();
+
+  // make sure PDF is reloaded when screen is focused
+  // (this makes sure PDF shows especially on Android)
+  useFocusEffect(
+    useCallback(() => {
+      webViewRef.current.reload();
+    }, [])
+  );
+
   let pdfUrl = source;
 
   if (Platform.OS === 'android') {
@@ -9,15 +21,12 @@ export default function PDFViewer({ source }) {
     // see https://github.com/facebook/react-native/issues/6488
     // route all PDF links through Google docs viewer
     pdfUrl = `https://docs.google.com/gview?embedded=true&url=${pdfUrl}`;
-    console.log(pdfUrl);
   }
 
   return (
     <WebView
+      ref={(ref) => (webViewRef.current = ref)}
       source={{ uri: pdfUrl }}
-      originWhitelist={['*']}
-      style={{ flex: 1 }}
-      scalesPageToFit={true}
     />
   );
 }
