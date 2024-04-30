b08f423d999da236ac26c6b9045bb8b3bc5bf545
Erik Behrends <behrends@users.noreply.github.com>
2024-04-30 12:10:12 +0200

    Upgrade React Native to v0.74

--- android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.kt
+++ android/app/src/main/java/de/dhbwloe/loerrach/campusapp/MainApplication.kt
@@ -3,11 +3,12 @@ package de.dhbwloe.loerrach.campusapp
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -27,6 +28,9 @@ class MainApplication : Application(), ReactApplication {
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
@@ -34,6 +38,5 @@ class MainApplication : Application(), ReactApplication {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
         }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
 }
