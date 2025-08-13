56a5aa537880929396442ccb00fe53ba97858e5f
Erik Behrends <behrends@users.noreply.github.com>
2025-08-13 14:19:52 +0200

    Remove legacy code

--- js/context/ColorSchemeContext.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const ColorSchemeContext = React.createContext(undefined);
--- js/util/DHBWActivityIndicator.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-
-export default () => (
-  <ActivityIndicator size="large" color={'#E2001A'} />
-);
